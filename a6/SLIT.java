b4b6c09593afd1a69424b1c57c8c3297aeb0652f
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-10-10 17:34:10 -0500

    Fixed count method

--- a6/SLIT.java
+++ a6/SLIT.java
@@ -14,54 +14,46 @@
 
 class SLIT {
 
-  /* given a matrix and a slit window, return T - R where:
-      + the slit window is a sub-matrix of the input matrix defined by:
-      - the row index r of its top-left corner
-      - the column index c of its top-left corner
-      - its width (i.e., number of columns)
-      - a height of 2 rows (always)
-      + T is the number of occurrences of 'T' in the slit window
-      + R is the number of occurrences of the rest of the bases (i.e., 'A',
-      'C', and 'G') in the slit window
-
-      For example, if the input matrix is:
-
-      TTTACTCT     and r = 5 then T = 1 and the method returns -7
-      CTTTTTGG         c = 3      R = 7
-      AGTTATCT         w = 4
-      TAATTTTT
-      TTTCCTCT
-      GTTCAGTA
-      TTTCACGG
-      TGCTTTTT
-  */
+  /*
+   * given a matrix and a slit window, return T - R where: + the slit window is a
+   * sub-matrix of the input matrix defined by: - the row index r of its top-left
+   * corner - the column index c of its top-left corner - its width (i.e., number
+   * of columns) - a height of 2 rows (always) + T is the number of occurrences of
+   * 'T' in the slit window + R is the number of occurrences of the rest of the
+   * bases (i.e., 'A', 'C', and 'G') in the slit window
+   * 
+   * For example, if the input matrix is:
+   * 
+   * TTTACTCT and r = 5 then T = 1 and the method returns -7 CTTTTTGG c = 3 R = 7
+   * AGTTATCT w = 4 TAATTTTT TTTCCTCT GTTCAGTA TTTCACGG TGCTTTTT
+   */
   static int count(char[][] mat, int r, int c, int w) {
     int numberT = 0;
     int nonNumberT = 0;
-    for(int i =r; i<r+2; i++){
-      for(int j=c; j<j+w; j++){
-        if(mat[i][j] == 'T'){
+    for (int i = r; i < r + 2; i++) {
+      for (int j = c; j < c + w; j++) {
+        if (mat[i][j] == 'T') {
           numberT++;
-        }
-        else{
+        } else {
           nonNumberT++;
         }
       }
     }
-    return numberT-nonNumberT;
+    return numberT - nonNumberT;
   }// count method
 
-  /* your implementation of the brute-force algorithm whose pseudocode is
-     given in the handout
-  */
+  /*
+   * your implementation of the brute-force algorithm whose pseudocode is given in
+   * the handout
+   */
   static int algorithm1(char[][] mat, int n) {
 
-    int slit =0;
+    int slit = 0;
 
-    for(int row =0; row<=n-2; row++){
-      for(int col=0; col<=n-1; col++){
-        for(int w=1; w<=n-col;w++){
-        slit=Math.max(slit,count(mat,row,col,w));
+    for (int row = 0; row <= n - 2; row++) {
+      for (int col = 0; col <= n - 1; col++) {
+        for (int w = 1; w <= n - col; w++) {
+          slit = Math.max(slit, count(mat, row, col, w));
         }
       }
     }
@@ -69,16 +61,18 @@ class SLIT {
     return slit;
   }// algorithm1 method
 
-  /* your implementation of the second algorithm, which you must design
-  */
-  static int algorithm2(char[][] m,int n) {
+  /*
+   * your implementation of the second algorithm, which you must design
+   */
+  static int algorithm2(char[][] m, int n) {
 
     /* To be completed */
 
     return -1;
   }// algorithm2 method
 
-  /* implement your helper method(s) below this point
-  */
+  /*
+   * implement your helper method(s) below this point
+   */
 
 }// SLIT class
