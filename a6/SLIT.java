8194daadaf92ab2475f4b29210156e20d3b56153
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-10-12 13:22:08 -0500

    changed some stuff

--- a6/SLIT.java
+++ a6/SLIT.java
@@ -76,25 +76,25 @@ class SLIT {
   /*
    * your implementation of the second algorithm, which you must design
    */
-  // static int algorithm2(char[][] m, int n) {
-  //   if(n == 0){
-  //     return 0;
-  //   } 
-  //   if(n == 1){
-  //     return m[1][1] == 'T' ? 1 : -1; 
-  //   }
-  //   return algorithm2helper(m, 0, 0, n);
-  // }// algorithm2 method
+  static int algorithm2(char[][] m, int n) {
+    if(n == 0){
+      return 0;
+    } 
+    if(n == 1){
+      return m[1][1] == 'T' ? 1 : -1; 
+    }
+    return algorithm2helper(m, 0, 0, n);
+  }// algorithm2 method
 
-  static int algorithm2(char[][] m, int n){
-    // int slit = 0;
-    // for (int row = 0; row <= n-2; row++){
-    //   for(int col = 0; col <= n-1; row++){
-    //     slit = Math.min
-    //   }
-    // }
-    return -1;
-  }//algorithm2 method
+  // static int algorithm2(char[][] m, int n){
+  //   // int slit = 0;
+  //   // for (int row = 0; row <= n-2; row++){
+  //   //   for(int col = 0; col <= n-1; row++){
+  //   //     slit = Math.min
+  //   //   }
+  //   // }
+  //   return -1;
+  // }//algorithm2 method
 
   /*
    * implement your helper method(s) below this point
@@ -102,21 +102,27 @@ class SLIT {
    
    //recursive method that returns 1/4 the array until we get a 2x2 matrix
    //this was getting difficult to make it even faster so going with an easier method first
-  //  static int algorithm2helper(char[][] m, int row, int col, int n){
-  //    //assuming that n is always even
-  //    if(n == 2){
-  //      //basecase
-  //      return count(m, row, col, n);
-  //    }
-  //   //  need to finish recursive function
-  //   //  checks each quarter of matrix
-  //    int nw = algorithm2helper(m, row, col, n/2);
-  //    int ne = algorithm2helper(m, row, n/2, n/2);
-  //    int sw = algorithm2helper(m, n/2, col, n/2);
-  //    int se = algorithm2helper(m, n/2, n/2, n/2);
-  //    int slit = Math.min(nw + ne, sw + se);
-  //    slit = Math.min(slit, al);
-  //  }
+   static int algorithm2helper(char[][] m, int row, int col, int n){
+     int middle = n/2;
+     //assuming that n is always even
+     if(n == 2){
+       //basecase
+       return count(m, row, col, n);
+     }
+     if(n % 2 != 0){
+      middle = n/2 + 1;
+     } 
+      //  need to finish recursive function
+      //  checks each quarter of matrix
+      int nw = algorithm2helper(m, row, col, middle);
+      int ne = algorithm2helper(m, row, middle, middle);
+      int sw = algorithm2helper(m, middle, col, middle);
+      int se = algorithm2helper(m, middle, middle, middle);
+      int slit = Math.max(nw + ne, sw + se);
+      slit = Math.min(slit, al);
+
+
+   }
 
   //May not need, may be able to make this even faster
   //  static int rowCount(char[] row, int n){
