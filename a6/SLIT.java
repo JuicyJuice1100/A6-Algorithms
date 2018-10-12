3610d2dc74c6b96c573bf7827fda063330e5dbd5
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-10-12 01:11:47 -0500

    Cleaned up algorithm1.  Tried to figure out algorithm2 but got stuck on how to do recursive call.

--- a6/SLIT.java
+++ a6/SLIT.java
@@ -14,32 +14,44 @@
 
 class SLIT {
 
-  /*
-   * given a matrix and a slit window, return T - R where: + the slit window is a
-   * sub-matrix of the input matrix defined by: - the row index r of its top-left
-   * corner - the column index c of its top-left corner - its width (i.e., number
-   * of columns) - a height of 2 rows (always) + T is the number of occurrences of
-   * 'T' in the slit window + R is the number of occurrences of the rest of the
-   * bases (i.e., 'A', 'C', and 'G') in the slit window
-   * 
-   * For example, if the input matrix is:
-   * 
-   * TTTACTCT and r = 5 then T = 1 and the method returns -7 CTTTTTGG c = 3 R = 7
-   * AGTTATCT w = 4 TAATTTTT TTTCCTCT GTTCAGTA TTTCACGG TGCTTTTT
-   */
+  /* given a matrix and a slit window, return T - R where:
+      + the slit window is a sub-matrix of the input matrix defined by:
+      - the row index r of its top-left corner
+      - the column index c of its top-left corner
+      - its width (i.e., number of columns)
+      - a height of 2 rows (always)
+      + T is the number of occurrences of 'T' in the slit window
+      + R is the number of occurrences of the rest of the bases (i.e., 'A',
+      'C', and 'G') in the slit window
+
+      For example, if the input matrix is:
+
+      TTTACTCT     and r = 5 then T = 1 and the method returns -6
+      CTTTTTGG         c = 3      R = 7
+      AGTTATCT         w = 4
+      TAATTTTT
+      TTTCCTCT
+      GTTCAGTA
+      TTTCACGG
+      TGCTTTTT
+  */
   static int count(char[][] mat, int r, int c, int w) {
-    int numberT = 0;
-    int nonNumberT = 0;
+    // int numberT = 0;
+    // int nonNumberT = 0;
+    int sum = 0;
     for (int i = r; i < r + 2; i++) {
       for (int j = c; j < c + w; j++) {
         if (mat[i][j] == 'T') {
-          numberT++;
+          // numberT++;
+          sum++;
         } else {
-          nonNumberT++;
+          // nonNumberT++;
+          sum--;
         }
       }
     }
-    return numberT - nonNumberT;
+    // return numberT - nonNumberT;
+    return sum;
   }// count method
 
   /*
@@ -64,15 +76,57 @@ class SLIT {
   /*
    * your implementation of the second algorithm, which you must design
    */
-  static int algorithm2(char[][] m, int n) {
+  // static int algorithm2(char[][] m, int n) {
+  //   if(n == 0){
+  //     return 0;
+  //   } 
+  //   if(n == 1){
+  //     return m[1][1] == 'T' ? 1 : -1; 
+  //   }
+  //   return algorithm2helper(m, 0, 0, n);
+  // }// algorithm2 method
 
-    /* To be completed */
-
+  static int algorithm2(char[][] m, int n){
+    // int slit = 0;
+    // for (int row = 0; row <= n-2; row++){
+    //   for(int col = 0; col <= n-1; row++){
+    //     slit = Math.min
+    //   }
+    // }
     return -1;
-  }// algorithm2 method
+  }//algorithm2 method
 
   /*
    * implement your helper method(s) below this point
    */
+   
+   //recursive method that returns 1/4 the array until we get a 2x2 matrix
+   //this was getting difficult to make it even faster so going with an easier method first
+  //  static int algorithm2helper(char[][] m, int row, int col, int n){
+  //    //assuming that n is always even
+  //    if(n == 2){
+  //      //basecase
+  //      return count(m, row, col, n);
+  //    }
+  //   //  need to finish recursive function
+  //   //  checks each quarter of matrix
+  //    int nw = algorithm2helper(m, row, col, n/2);
+  //    int ne = algorithm2helper(m, row, n/2, n/2);
+  //    int sw = algorithm2helper(m, n/2, col, n/2);
+  //    int se = algorithm2helper(m, n/2, n/2, n/2);
+  //    int slit = Math.min(nw + ne, sw + se);
+  //    slit = Math.min(slit, al);
+  //  }
+
+  //May not need, may be able to make this even faster
+  //  static int rowCount(char[] row, int n){
+  //    int count = 0;
+  //    for(int col = 0; col < n; col++){
+  //      if(row[col] != 'T'){
+  //        count++;
+  //      }
+  //    }
+  //    return count;
+  //  }
 
 }// SLIT class
